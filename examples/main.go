7dcad264a136798d565d1a260d5c137a0af437da
Alexander Savin <alex@savin.nyc>
2020-05-09 22:12:14 -0400

    Added missed package

--- examples/main.go
+++ examples/main.go
@@ -1,24 +1,26 @@
 package main
 
 import (
-    "github.com/alex-savin/mysubaru"
+	"time"
+
+	"github.com/alex-savin/mysubaru"
 )
 
 func main() {
-    car, _ := mysubaru.New()
+	car, _ := mysubaru.New()
 
-    car.R().Unlock()
-    car.R().Lock()
+	car.R().Unlock()
+	car.R().Lock()
 
-    car.R().GetLocation()
-    car.R().GetHealth()
-    car.R().GetClimateSettings()
+	car.R().GetLocation()
+	car.R().GetHealth()
+	car.R().GetClimateSettings()
 
-    car.R().StartLightHorn()
-    time.Sleep(5 * time.Second)
-    car.R().StopLightHorn()
+	car.R().StartLightHorn()
+	time.Sleep(5 * time.Second)
+	car.R().StopLightHorn()
 
-    car.R().StartEngine()
-    time.Sleep(10 * time.Second)
-    car.R().StopEngine()
+	car.R().StartEngine()
+	time.Sleep(10 * time.Second)
+	car.R().StopEngine()
 }
